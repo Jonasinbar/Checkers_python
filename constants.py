73b4b00e26ec36b3c9b5be44407e92730c67a508
Jonas Inbar <jonasinbar@gmail.com>
2024-02-04 11:35:34 +0200

    reorganize files, settings.yaml for exe

--- constants.py
+++ constants.py
@@ -1,17 +1,45 @@
 import yaml
 
-# Load the settings from the YAML file
-with open('settings.yaml', 'r') as yaml_file:
-    settings = yaml.safe_load(yaml_file)
+SIZE_BOARD = None
+NUMBER_OF_LINES_OF_DOTS_IN_START = None
+PLAYER_1_COLOR = None
+PLAYER_2_COLOR = None
 
-# Assign constants from the settings
-SIZE_BOARD = settings['SIZE_BOARD']
-NUMBER_OF_LINES_OF_DOTS_IN_START = settings['NUMBER_OF_LINES_OF_DOTS_IN_START']
-PLAYER_1_COLOR = tuple(settings['PLAYER_1_COLOR'])
-PLAYER_2_COLOR = tuple(settings['PLAYER_2_COLOR'])
-PORT = settings['PORT']
-IP_ADDRESS_SERVER = settings['IP_ADDRESS_SERVER']
-IP_ADDRESS_CLIENT = settings['IP_ADDRESS_CLIENT']
+try:
+    with open('settings_server.yaml', 'r') as yaml_file:
+        settings_server = yaml.safe_load(yaml_file)
+        SIZE_BOARD = settings_server['SIZE_BOARD']
+        NUMBER_OF_LINES_OF_DOTS_IN_START = settings_server['NUMBER_OF_LINES_OF_DOTS_IN_START']
+        PLAYER_1_COLOR = settings_server['PLAYER_1_COLOR']
+        PLAYER_2_COLOR = settings_server['PLAYER_2_COLOR']
+        PORT = settings_server['PORT']
+        IP_ADDRESS_SERVER = settings_server['IP_ADDRESS_SERVER']
+
+except FileNotFoundError:
+    print("settings server not here")
+    pass
+
+try:
+    # Load the settings_server from the YAML file
+    with open('settings_client.yaml', 'r') as yaml_file:
+        settings_client = yaml.safe_load(yaml_file)
+        IP_ADDRESS_CLIENT = settings_client['IP_ADDRESS_CLIENT']
+        PORT = settings_client['PORT']
+except FileNotFoundError:
+    print("settings client not here")
+    pass
+
+try:
+    with open('settings.yaml', 'r') as yaml_file:
+        settings = yaml.safe_load(yaml_file)
+        SIZE_BOARD = settings['SIZE_BOARD']
+        NUMBER_OF_LINES_OF_DOTS_IN_START = settings['NUMBER_OF_LINES_OF_DOTS_IN_START']
+        PLAYER_1_COLOR = settings['PLAYER_1_COLOR']
+        PLAYER_2_COLOR = settings['PLAYER_2_COLOR']
+
+except FileNotFoundError:
+    print("settings  not here")
+    pass
 
 WIDTH = 1500
 HEIGHT = 800
@@ -33,4 +61,5 @@ CROWN_PATH = "assets\\images\\crown.png"
 
 UP_LEFT = (MARGIN, MARGIN)
 DOWN_RIGHT = (min(WIDTH, HEIGHT) - MARGIN, min(WIDTH, HEIGHT) - MARGIN)
-SQUARE_SIZE = (DOWN_RIGHT[0] - UP_LEFT[0]) / SIZE_BOARD
+if SIZE_BOARD:
+    SQUARE_SIZE = (DOWN_RIGHT[0] - UP_LEFT[0]) / SIZE_BOARD
